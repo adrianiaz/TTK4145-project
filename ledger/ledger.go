716c55b8618a7c7c38f0efffe23911c164f1a13f
Henrik Elde <h.a.e@hotmail.no>
2024-03-08 18:29:21 +0100

    Add ledger struct

--- ledger/ledger.go
+++ ledger/ledger.go
@@ -1,1 +1,25 @@
 package ledger
+
+//import ("encoding/json", log, fmt)
+
+type ledger struct {
+	BackupMaster []string `json:"backupMaster"` //maby int instead og string if use of IP?
+	Alive        []string `json:"alive"`
+	Orders       []string `json:"orders"`
+}
+
+// Exampel of serializing (Marshal):
+// type Person struct {
+//     Navn  string `json:"navn"`
+//     Alder int    `json:"alder"`
+// }
+
+// func main() {
+//     p := Person{"Ola Nordmann", 30}
+//     pJson, err := json.Marshal(p)
+//     if err != nil {
+//         fmt.Println(err)
+//     }
+//     fmt.Println(string(pJson)) // Output: {"navn":"Ola Nordmann","alder":30}
+// }
+//
